301e8951c84ab310ee61c4f409bf818c1678b9f0
BeluNgas <98247916+BeluNgas@users.noreply.github.com>
2022-02-19 13:54:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -34,20 +34,3 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 })
-
-global.sleave = '◪ Sayonara Sussy Baka @tag\n◪ Leave from group:\n@subject\n\n│\n└─ ❏ Nomor: @number\n😷😷~~'
-global.spromote = '*P R O M O T E  D E T E C T E D*\n```Nomor :``` @number\n```User :``` @tag\n```Group :``` @subject\n_Selamat jabatan kamu admin_'
-global.sdemote = '*D E M O T E  D E T E C T E D*\n```Nomor :``` @number\n```User :``` @tag\n```Group :``` @subject\n_Kamu jadi member_'
-global.alphaVersion = [2, 2143, 3]
-global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright("Update 'config.js'"))
-	delete require.cache[file]
-	require(file)
-})
